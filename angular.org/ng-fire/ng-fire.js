176850834483446bce6a91d2f0cac8b241b61ed0
zenithtekla <zenithtekla@gmail.com>
2016-01-15 22:38:59 +0000

    r15 refine ng-fire.js

--- angular.org/ng-fire/ng-fire.js
+++ angular.org/ng-fire/ng-fire.js
@@ -1,47 +1,28 @@
 var app = angular.module("sampleApp", ["firebase"]);
-app.factory("ListWithTotal", ["$firebaseArray",
-  function($firebaseArray) {
-    // create a new service based on $firebaseArray
-    var ListWithTotal = $firebaseArray.$extend({
-      getTotal: function() {
-        var total = 0;
-        // the array data is located in this.$list
-        angular.forEach(this.$list, function(rec) {
-          total += rec.amount;
-        });
-        return total;
-      }
-    });
-    return function(listRef) {
-      // create an instance of ListWithTotal (the new operator is required)
-      return new ListWithTotal(listRef);
-    }
-  }
-]);
-app.controller("SampleCtrl", function($scope, $firebaseAuth, ListWithTotal, $firebaseArray) {
-    var ref = new Firebase("https://zetek.firebaseio.com/");
+
+app.controller("SampleCtrl", function($scope, $firebaseAuth, $firebaseArray) {
+    var ref = new Firebase("https://zetek.firebaseio.com");
     
     // create an instance of the authentication service
     var auth = $firebaseAuth(ref);
     // login with Google
     auth.$authWithOAuthPopup("google").then(function(authData) {
         console.log("Logged in as:", authData.uid);
+            // create a synchronized array
+        $scope.messages = $firebaseArray(ref);
+    
+        // add new items to the array
+        // the message is automatically added to our Firebase database!
+          
+        $scope.addMessage = function() {
+            $scope.messages.$add({
+                text: $scope.newMessageText
+            });
+        };
     }).catch(function(error) {
         console.log("Authentication failed:", error);
     });
     
-    // create a synchronized array
-    $scope.messages = $firebaseArray(ref);
-    $scope.total = ListWithTotal(ref);
-      // add new items to the array
-      // the message is automatically added to our Firebase database!
-      
-    $scope.addMessage = function() {
-        $scope.messages.$add({
-          total: $scope.total,
-          text: $scope.newMessageText
-        });
-    };
     /*var authData = ref.getAuth();
     if (authData) {
         console.log("User " + authData.uid + " is logged in with " + authData.provider);
